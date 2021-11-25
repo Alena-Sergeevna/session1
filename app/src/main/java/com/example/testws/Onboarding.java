bbed5d5e49588b94d0093f38bdb98c7e5fae4e81
Alena-Sergeevna <alenasergeevnaal@gmail.com>
2021-11-26 01:21:55 +0800

    session 1/ session1V2

--- app/src/main/java/com/example/testws/Onboarding.java
+++ app/src/main/java/com/example/testws/Onboarding.java
@@ -3,14 +3,17 @@ package com.example.testws;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.content.Intent;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.testws.ui.login.LoginActivity;
+
 
 public class Onboarding extends AppCompatActivity {
 
-    private Button btn_alert;
+//    private Button btn_alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +21,25 @@ public class Onboarding extends AppCompatActivity {
         setContentView(R.layout.activity_onboarding);
 
     }
+    public void onBtnAlert(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
 
-    public void addListenerOnButton(View view) {
-        btn_alert = (Button) findViewById(R.id.btn_alert);
-
-        btn_alert.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(".ui.login.LoginActivity");
-                        startActivity(intent);
-                    }
-                }
-        );
     }
+
+//    public void addListenerOnButton(View view) {
+//        btn_alert = (Button) findViewById(R.id.btn_alert);
+//
+//        btn_alert.setOnClickListener(
+//                new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Intent intent = new Intent(".ui.login.LoginActivity");
+//                        startActivity(intent);
+//                    }
+//                }
+//        );
+//    }
+
+
 }
