5f0cd179a22d6910c4c0c0e06b8cd78407cfcb93
Alena-Sergeevna <alenasergeevnaal@gmail.com>
2021-11-25 21:08:20 +0800

    session 1/ login

--- app/src/main/java/com/example/testws/Lounch_screen.java
+++ app/src/main/java/com/example/testws/Lounch_screen.java
@@ -1,6 +1,7 @@
 package com.example.testws;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 
 import android.os.Bundle;
 
@@ -10,5 +11,32 @@ public class Lounch_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread logoTimer = new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    int logoTimer = 0;
+                    while(logoTimer < 5000)
+                    {
+                        sleep(100);
+                        logoTimer = logoTimer +100;
+                    };
+                    startActivity(new Intent("com.tutorial.CLEARSCREEN"));
+                }
+                catch (InterruptedException e)
+                {
+                    // TODO: автоматически сгенерированный блок catch.
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    finish();
+                }
+            }
+        };
+        logoTimer.start();
     }
 }
--- app/src/main/java/com/example/testws/Onboarding.java
+++ app/src/main/java/com/example/testws/Onboarding.java
@@ -1,14 +1,35 @@
 package com.example.testws;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class Onboarding extends AppCompatActivity {
+
+    private Button btn_alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+    }
+
+    public void addListenerOnButton(View view) {
+        btn_alert = (Button) findViewById(R.id.btn_alert);
+
+        btn_alert.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(".ui.login.LoginActivity");
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
